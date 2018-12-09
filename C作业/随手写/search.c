62e90aec15e3b2b76438b67fe7b13b23d444306c
Zhongdong Wang <44559384+Rtx8080Ti@users.noreply.github.com>
2018-12-09 22:23:25 +0800

    Update search.c

--- C作业/随手写/search.c
+++ C作业/随手写/search.c
@@ -7,22 +7,22 @@
 //
 
 #include <stdio.h>
-int search(int key,int a[],int len){//search 函数	//search函数(寻找的数字，数组a[]，数组长度）
+int search(int key,int a[],int len){    //search函数(寻找的数字，数组a[]，数组长度）
     int ret = -1;  //中间值
-    for (int i=0;i<len;i++){  //遍历数组
-        if (key == a[i]){	//如何数字等于key
-            ret = i;		//该数字在数组a 中的位置赋值给ret
-            break;		//跳出遍历
+    for (int i=0;i<len;i++){    //遍历数组
+        if (key == a[i]){	    //如何数字等于key
+            ret = i;		    //该数字在数组a 中的位置赋值给ret
+            break;              //跳出遍历
         }
     }
-    return ret;			//返回ret
+    return ret;		        	//返回ret
 }
 int main()
 {
     int a[]={1,2,4,5,6,7,8,1,22,33,42,56,21,4,};	//数组a
     int k=42;
     int r=search (k,a,sizeof(a)/sizeof(a[0]));		//返回值ret赋值给r
-        printf ("%d\n",r);				//输出位数
+        printf ("%d\n",r);			            	//输出位数
     
     return 0;
     }
